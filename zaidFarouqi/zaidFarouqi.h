ca58a09e6d547b37fc33705f25e7089d61c42f78
zaid farouqi <>
2020-03-25 10:52:07 +0200

    Initial project setup

---
+++ zaidFarouqi/zaidFarouqi.h
@@ -0,0 +1,19 @@
+//
+//  zaidFarouqi.h
+//  zaidFarouqi
+//
+//  Created by zaid farouqi on 3/25/20.
+//  Copyright © 2020 zaid farouqi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for zaidFarouqi.
+FOUNDATION_EXPORT double zaidFarouqiVersionNumber;
+
+//! Project version string for zaidFarouqi.
+FOUNDATION_EXPORT const unsigned char zaidFarouqiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <zaidFarouqi/PublicHeader.h>
+
+
